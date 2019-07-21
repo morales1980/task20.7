c667c96f03e09b0e700568284f28ec6400122974
Darek.Szotowicz <dariusz.szotowicz@protonmail.com>
2019-07-21 02:50:19 +0200

    task finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: 'style-loader'},
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
+module.exports = (env) => {
+  const environment = env;
+
+  return {
+    mode: environment,
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'app.'+ environment + '.bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          use: [
+            { loader: 'style-loader'},
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true
+              }
             }
-          }
-        ]
-      }
-    ]
-  }
+          ]
+        }
+      ]
+    }
+  };
 };
